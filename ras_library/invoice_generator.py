b981598bfed7c972635cdb1cf6535b1db1f8ed08
Reynaldo Arteaga <reynaldo@solaranalytics.com.au>
2018-03-11 14:29:19 +1000

    Added a very basic html file which will generate the invoice. Added a python file to generate the pdf invoice based on the html file with specific parameters.

---
+++ ras_library/invoice_generator.py
@@ -0,0 +1,25 @@
+'''
+    Invoice generator based on HTML file and input parameters
+
+    Author: Reynaldo Arteaga
+'''
+
+from jinja2 import Environment, FileSystemLoader
+from weasyprint import HTML
+
+
+def generate_invoice(html_file, template_vars):
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template(html_file)
+
+    html_out = template.render(template_vars)
+
+    HTML(string=html_out).write_pdf("ras_invoice_1.pdf")
+
+if __name__ == '__main__':
+    html_file = "invoice_template.html"
+    template_vars = {
+        'paragraph':
+            ['first paragraph', 'second paragraph', 'third paragraph']
+    }
+    generate_invoice(html_file, template_vars)

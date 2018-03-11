8383d4bd457387313c755409b2411260bf197e8e
Reynaldo Arteaga <reynaldo@solaranalytics.com.au>
2018-03-11 15:33:39 +1000

    Passing in all variables that will be used in the first iteration of invoices.

--- ras_library/invoice_generator.py
+++ ras_library/invoice_generator.py
@@ -2,24 +2,60 @@
     Invoice generator based on HTML file and input parameters
 
     Author: Reynaldo Arteaga
+    Date: March 2018
 '''
 
 from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML
 
 
-def generate_invoice(html_file, template_vars):
-    env = Environment(loader=FileSystemLoader('.'))
-    template = env.get_template(html_file)
+def generate_invoice(html_file, template_vars, pdf_file):
+    '''
+        Arguments:
+            html_file (str):        The name (incl. extension) of the HTML file
+            template_vars (dict):   All parameters used in the HTML render
+            pdf_file (str):         The name (incl. extension) of the pdf file
+        Returns:
+            success (bool):         True or false whether the pdf was generated
 
-    html_out = template.render(template_vars)
+    '''
+    success = False
+    try:
+        env = Environment(loader=FileSystemLoader('.'))
+        template = env.get_template(html_file)
 
-    HTML(string=html_out).write_pdf("ras_invoice_1.pdf")
+        html_out = template.render(template_vars)
+
+        HTML(string=html_out).write_pdf(pdf_file)
+
+        success = True
+        print('PDF Generated {}'.format(pdf_file))
+
+    except Exception as error:
+        print(
+            'PDF Generation failed for HTML: {}, Variables: {} and PDF: {}. '
+            'Error: {}'.format(html_file, template_vars, pdf_file, error))
+    
+    return success
+
+
 
 if __name__ == '__main__':
     html_file = "invoice_template.html"
     template_vars = {
-        'paragraphs':
-            ['first paragraph', 'second paragraph', 'third paragraph']
+        'bill_id': 1,
+        'date_created': '2018-3-11',
+        'due_date': '2018-3-11',
+        'labour_cost': 30.0,
+        'parts_cost': 20.50,
+        'total_before_tax': 50.50,
+        'tax_percentage': 13.0,
+        'tax_total': 6.565,
+        'total_after_tax': 57.065,
+        'description': 'We changed the oil',
+        'parts_replaced': ['oil', 'windshield fluid'],
+        'customer_id': 14,
+        'payment_status': 'unpaid',
     }
-    generate_invoice(html_file, template_vars)
+    pdf_file = "ras_invoice_1.pdf"
+    generate_invoice(html_file, template_vars, pdf_file)
